1a6d47fdc6648986ae27fc6419fb807efc974fb2
Shubham Dhabhai <shubham.dhabhai@gmail.com>
2018-02-19 15:47:22 +0530

    added default icon and color for notification

--- app/src/main/java/io/chatcamp/app/ChatCampAppFirebaseMessagingService.java
+++ app/src/main/java/io/chatcamp/app/ChatCampAppFirebaseMessagingService.java
@@ -1,10 +1,15 @@
 package io.chatcamp.app;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by ChatCamp Team on 07/12/17.
  */
 
 public class ChatCampAppFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
 }
