7e7d1a1f899d07d3c96686e9e0be95649ae390d7
Shubham Dhabhai <shubham.dhabhai@gmail.com>
2018-05-23 01:27:10 +0530

    removed chatkit and added it as library

--- chatkit/src/main/java/com/chatcamp/uikit/utils/HeaderViewClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.chatcamp.uikit.utils;
-
-/**
- * Created by shubhamdhabhai on 17/05/18.
- */
-
-public interface HeaderViewClickListener {
-    boolean onHeaderViewClicked(String channelId, boolean isOneToOneConversation, String otherParticipantId);
-}
