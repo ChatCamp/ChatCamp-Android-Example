3b1a9b496da57b0926a94bec9a4d549477682128
Shubham Dhabhai <shubham.dhabhai@gmail.com>
2018-05-23 09:31:54 +0530

    removed committed code in listactivity

--- app/src/main/java/io/chatcamp/app/ListActivity.java
+++ app/src/main/java/io/chatcamp/app/ListActivity.java
@@ -13,108 +13,28 @@ import io.chatcamp.app.setting.SettingFragment;
 
 public class ListActivity extends AppCompatActivity {
 
-//    private ChannelList channelList;
-//    private TabLayout mTabLayout;
-//    private TextView mTextMessage;
-//    private GroupChannelListQuery.ParticipantState groupFilter;
     private FloatingActionButton mChannelCreate;
-//    private Timer timer;
-
-//    private TabLayout.OnTabSelectedListener mTabLayoutOnClickListener = new TabLayout.OnTabSelectedListener() {
-//        @Override
-//        public void onTabSelected(TabLayout.Tab tab) {
-//            Log.d("APP", "Hello: " + tab.getPosition());
-//            switch (tab.getPosition()) {
-//                case 0:
-//                    groupFilter = GroupChannelListQuery.ParticipantState.ALL;
-//                    handleNavigationGroupChannels();
-//                    return;
-//                case 1:
-//                    groupFilter = GroupChannelListQuery.ParticipantState.INVITED;
-//                    handleNavigationGroupChannels();
-//                    return;
-//                case 2:
-//                    groupFilter = GroupChannelListQuery.ParticipantState.ACCEPTED;
-//                    handleNavigationGroupChannels();
-//                    return;
-//            }
-//        }
-//
-//        @Override
-//        public void onTabUnselected(TabLayout.Tab tab) {
-//
-//        }
-//
-//        @Override
-//        public void onTabReselected(TabLayout.Tab tab) {
-//
-//        }
-//    };
 
     private BottomNavigationView navigation;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-        //Initializing the tablayout
-//        mTabLayout = (TabLayout) findViewById(R.id.group_navigation);
-//
-//        channelList = (ChannelList) findViewById(R.id.rv_channels_list);
-//
-//        mTextMessage = (TextView) findViewById(R.id.message);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mChannelCreate = (FloatingActionButton) findViewById(R.id.floating_button_create);
         mChannelCreate.setOnClickListener(mChannelCreateClickListener);
-//        mTabLayout.addOnTabSelectedListener(mTabLayoutOnClickListener);
-//        channelList.setChannelClickListener(new ChannelAdapter.ChannelClickedListener() {
-//            @Override
-//            public void onClick(BaseChannel baseChannel) {
-//                Intent intent = new Intent(getApplicationContext(), ConversationActivity.class);
-//                intent.putExtra("channelType", baseChannel.isOpenChannel() ? "open" : "group");
-//                intent.putExtra("participantState", groupFilter.name());
-//                intent.putExtra("channelId", baseChannel.getId());
-//                startActivity(intent);
-//            }
-//        });
         handleNavigationOpenChannels();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        timer = new Timer();
-//        timer.schedule(new UpdateListTask(), 10000, 4000);
-    }
 
     private void handleNavigationGroupChannels() {
-//        channelList.setChannelType(BaseChannel.ChannelType.GROUP, groupFilter);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new GroupChannelListFragment()).commit();
     }
 
     private void handleNavigationOpenChannels() {
-//        channelList.setChannelType(BaseChannel.ChannelType.OPEN, null);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new OpenChannelListFragment()).commit();
     }
-
-
-//    class UpdateListTask extends TimerTask {
-//        @Override
-//        public void run() {
-//            if (navigation.getSelectedItemId() == R.id.navigation_group_channels) {
-//                //TODO add this task in the view itself so that user dont have to make this.
-//            }
-//        }
-//    }
-
-    @Override
-    protected void onStop() {
-//        timer.cancel();
-//        timer = null;
-        super.onStop();
-    }
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -123,26 +43,15 @@ public class ListActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_open_channels:
-//                    mTextMessage.setText(R.string.title_open_channels);
                     handleNavigationOpenChannels();
                     return true;
                 case R.id.navigation_group_channels:
-//                    mTextMessage.setText(R.string.title_group_channels);
-//                    mTabLayout.setVisibility(View.VISIBLE);
-//                    mTabLayout.removeAllTabs();
-//                    mTabLayout.addTab(mTabLayout.newTab().setText(R.string.group_all_channels));
-//                    mTabLayout.addTab(mTabLayout.newTab().setText(R.string.group_invited_channels));
-//                    mTabLayout.addTab(mTabLayout.newTab().setText(R.string.group_accepted_channels));
-//                    getSupportActionBar().setTitle("Group Channels");
                      handleNavigationGroupChannels();
                     return true;
                 case R.id.navigation_user_list:
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, new UserListFragment()).commit();
-//                    startActivity(new Intent(ListActivity.this, UserListFragment.class));
                     return true;
                 case R.id.navigation_settings:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    startActivity(new Intent(ListActivity.this, SettingFragment.class));
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, new SettingFragment()).commit();
                     return true;
             }
