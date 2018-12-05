9158cfc500fa62bff7eff35f88c12defc3b0dcf6
Shubham Dhabhai <shubham.dhabhai@gmail.com>
2018-12-05 13:46:27 +0530

    show image in notification

--- app/src/main/java/io/chatcamp/app/ChatCampAppFirebaseMessagingService.java
+++ app/src/main/java/io/chatcamp/app/ChatCampAppFirebaseMessagingService.java
@@ -6,16 +6,24 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import io.chatcamp.sdk.GroupChannelListQuery;
 import io.chatcamp.sdk.Message;
@@ -48,49 +56,111 @@ public class ChatCampAppFirebaseMessagingService extends FirebaseMessagingServic
             // this notification is not sent from Chatcamp
             return;
         }
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        final String CHANNEL_ID = "CHANNEL_ID";
-        if (Build.VERSION.SDK_INT >= 26) {  // Build.VERSION_CODES.O
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "CHANNEL_NAME", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(mChannel);
+        new SendNotification(context, channelId, channelType, message).execute();
+
+    }
+
+
+    private static class SendNotification extends AsyncTask<Void, Void, Bitmap> {
+
+        Context context;
+        String channelId;
+        String channelType;
+        Message message;
+
+        public SendNotification(Context context, String channelId, String channelType, Message message) {
+            super();
+            this.context = context;
+            this.channelId = channelId;
+            this.channelType = channelType;
+            this.message = message;
         }
 
-        Intent intent = new Intent(context, ConversationActivity.class);
-        String participantState = GroupChannelListQuery.ParticipantState.ALL.name();
-        intent.putExtra("channelId", channelId);
-        intent.putExtra("channelType", channelType);
-        intent.putExtra("participantState", participantState);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.icon_default_contact)
-                .setColor(Color.parseColor("#7469C4"))  // small icon background color
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_default_contact))
-                .setContentTitle(message.getUser().getDisplayName())
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setPriority(Notification.PRIORITY_MAX)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setContentIntent(pendingIntent);
+            InputStream in;
+            if(message.getUser().getAvatarUrl() != null) {
+                try {
 
-        if (message.getType().equals("attachment")) {
-            if (message.getAttachment().getType().contains("image")) {
-                notificationBuilder.setContentText("Image");
-            } else if (message.getAttachment().getType().contains("video")) {
-                notificationBuilder.setContentText("video");
-            }  else if (message.getAttachment().getType().contains("application") || message.getAttachment().getType().contains("css") ||
-                    message.getAttachment().getType().contains("csv") || message.getAttachment().getType().contains("text")) {
-                notificationBuilder.setContentText("document");
+                    URL url = new URL(message.getUser().getAvatarUrl());
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoInput(true);
+                    connection.connect();
+                    in = connection.getInputStream();
+                    Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                    return myBitmap;
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } else if(message.getType().equals("text")) {
-            notificationBuilder.setContentText(message.getText());
-        } else {
-            notificationBuilder.setContentText("new Message");
+            return null;
         }
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        @Override
+        protected void onPostExecute(Bitmap result) {
+
+            super.onPostExecute(result);
+            if(result == null) {
+                result = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon_default_contact);
+            }
+            try {
+
+                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                final String CHANNEL_ID = "CHANNEL_ID";
+                if (Build.VERSION.SDK_INT >= 26) {  // Build.VERSION_CODES.O
+                    NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "CHANNEL_NAME", NotificationManager.IMPORTANCE_HIGH);
+                    notificationManager.createNotificationChannel(mChannel);
+                }
+
+                Intent intent = new Intent(context, ConversationActivity.class);
+                String participantState = GroupChannelListQuery.ParticipantState.ALL.name();
+                intent.putExtra("channelId", channelId);
+                intent.putExtra("channelType", channelType);
+                intent.putExtra("participantState", participantState);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ifly_blue_icon)
+                        .setColor(Color.parseColor("#7469C4"))  // small icon background color
+                        .setLargeIcon(result)
+                        .setContentTitle(message.getUser().getDisplayName())
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setPriority(Notification.PRIORITY_MAX)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setContentIntent(pendingIntent);
+
+                if (message.getType().equals("attachment")) {
+                    if (message.getAttachment().getType().contains("image")) {
+                        notificationBuilder.setContentText("Image");
+                    } else if (message.getAttachment().getType().contains("video")) {
+                        notificationBuilder.setContentText("video");
+                    }  else if (message.getAttachment().getType().contains("application") || message.getAttachment().getType().contains("css") ||
+                            message.getAttachment().getType().contains("csv") || message.getAttachment().getType().contains("text")) {
+                        notificationBuilder.setContentText("document");
+                    }
+                } else if(message.getType().equals("text")) {
+                    notificationBuilder.setContentText(message.getText());
+                } else {
+                    notificationBuilder.setContentText("new Message");
+                }
+
+                notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
